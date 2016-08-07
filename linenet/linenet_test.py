746f547d27ba27eedaf5ffc97e550507b4db0748
byungsook <contact.byungsoo@gmail.com>
2016-08-07 12:33:58 +0200

    update linenet_test

--- linenet/linenet_test.py
+++ linenet/linenet_test.py
@@ -22,18 +22,16 @@ import linenet_model
 
 # parameters
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('test_name', 'test_p2_i10_n6400',
-                           """test name""")
-tf.app.flags.DEFINE_string('test_dir', 'test/' + FLAGS.test_name,
+tf.app.flags.DEFINE_string('test_dir', 'test/test_p2_i10_n6400',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_string('pretrained_model_checkpoint_path', 'log/ratio_test/10_100000_30000_0.01/linenet.ckpt',
                            """If specified, restore this pretrained model.""")
-tf.app.flags.DEFINE_integer('max_images', FLAGS.batch_size,
+tf.app.flags.DEFINE_integer('max_images', 3,
                             """max # images to save.""")
-tf.app.flags.DEFINE_integer('num_eval', 640,
+tf.app.flags.DEFINE_integer('num_eval', 6400,
                             """# images for evaluation""")
-tf.app.flags.DEFINE_integer('num_line_ext', 5,
+tf.app.flags.DEFINE_integer('num_line_ext', 10,
                             """# iteration for convergence of line extraction""")
 
 
@@ -58,7 +56,7 @@ def test_iter():
 
 
         # Build the summary writer
-        summary_writer = [tf.train.SummaryWriter(FLAGS.test_dir + '/%d' % i, g) for i in xrange(FLAGS.num_line_ext)]
+        summary_writer = [tf.train.SummaryWriter(FLAGS.test_dir + '/%02d' % i, g) for i in xrange(FLAGS.num_line_ext)]
 
         # summary at each iteration
         loss_ph = [tf.placeholder(tf.float32)] * FLAGS.num_line_ext
@@ -67,8 +65,10 @@ def test_iter():
         loss_avg = tf.placeholder(tf.float32)
         loss_avg_summary = tf.scalar_summary('l2 loss (avg)', loss_avg)
 
+        x_summary = tf.image_summary('x', x, max_images=FLAGS.max_images)
+        y_summary = tf.image_summary('y', y, max_images=FLAGS.max_images)
         y_hat_ph = tf.placeholder(tf.float32)
-        y_hat_summary = tf.image_summary('y_hat_ph', y_hat_ph, max_images=1)
+        y_hat_summary = tf.image_summary('y_hat_ph', y_hat_ph, max_images=FLAGS.max_images)
 
         
         # Start evaluation
@@ -99,49 +99,23 @@ def test_iter():
                     loss_summary_str = sess.run(loss_summary[i], feed_dict={loss_ph[i]: loss_value})
                     summary_writer[i].add_summary(loss_summary_str, step)
 
-                    y_hat_summary_str = sess.run(y_hat_summary, feed_dict={y_hat_ph: y_hat_value})
-                    summary_writer[i].add_summary(y_hat_summary_str, step)
+                    if step == 0:
+                        x_summary_str, y_summary_str, y_hat_summary_str = sess.run([x_summary, y_summary, y_hat_summary], 
+                            feed_dict={x: x_batch, y: y_batch, y_hat_ph: y_hat_value})
+                        summary_writer[i].add_summary(x_summary_str, step)
+                        summary_writer[i].add_summary(y_summary_str, step)
+                        summary_writer[i].add_summary(y_hat_summary_str, step)
+
 
                     linenet_data.new_x_from_y_with_p(x_batch, y_hat_value, p_batch)
-                
-
-                # loss_summary_str = sess.run(loss_summary, feed_dict={loss_ph: loss_value})
-                # summary_writer.add_summary(loss_summary_str, step)
-
-                # loss_summary_str, x_no_p_summary_str, x_summary_str, y_summary_str, y_hat_summary_str = sess.run(
-                #     [loss_summary, x_no_p_summary, x_summary, y_summary, y_hat_summary],
-                #     feed_dict={loss_ph: loss_value,
-                #                x_no_p: x_no_p_batch, x: x_batch, y: y_batch, y_hat_ph: y_hat_value})
-
-                # summary_writer.add_summary(loss_summary_str, step)
-
-                # x_no_p_summary_tmp = tf.Summary()
-                # x_summary_tmp = tf.Summary()
-                # y_summary_tmp = tf.Summary()
-                # y_hat_summary_tmp = tf.Summary()
-                # x_no_p_summary_tmp.ParseFromString(x_no_p_summary_str)
-                # x_summary_tmp.ParseFromString(x_summary_str)
-                # y_summary_tmp.ParseFromString(y_summary_str)
-                # y_hat_summary_tmp.ParseFromString(y_hat_summary_str)
-                # for i in xrange(FLAGS.max_images):
-                #     new_tag = '%06d/%03d' % (step, i)
-                #     x_no_p_summary_tmp.value[i].tag = new_tag
-                #     x_summary_tmp.value[i].tag = new_tag
-                #     y_summary_tmp.value[i].tag = new_tag
-                #     y_hat_summary_tmp.value[i].tag = new_tag
-
-                # summary_x_no_p_writer.add_summary(x_no_p_summary_tmp, step)
-                # summary_x_writer.add_summary(x_summary_tmp, step)
-                # summary_y_writer.add_summary(y_summary_tmp, step)
-                # summary_y_hat_writer.add_summary(y_hat_summary_tmp, step)
 
             for i in xrange(FLAGS.num_line_ext):
                 # Compute precision
                 loss_avg_ = total_loss[i] / num_iter / FLAGS.num_eval
-                print('%s: %d, loss avg = %.3f' % (datetime.now(), i, loss_avg_))
+                print('%s: %d, loss avg = %.7f' % (datetime.now(), i, loss_avg_))
 
                 loss_avg_summary_str = sess.run(loss_avg_summary, feed_dict={loss_avg: loss_avg_})
-                summary_writer[i].add_summary(loss_avg_summary_str, i)
+                summary_writer[0].add_summary(loss_avg_summary_str, i)
                 
                 #loss_avg_summary_str = sess.run(loss_avg_summary[i], feed_dict={loss_avg[i]: loss_avg_})
                 #g_step = tf.train.global_step(sess, global_step)

d902721b9b57a021d6f80026dd790c9e746e7cb3
byungsook <contact.byungsoo@gmail.com>
2016-12-14 18:16:43 +0100

    update postprocessing

--- graphcut_vec_chinese.py
+++ graphcut_vec_chinese.py
@@ -39,7 +39,7 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('test_dir', 'test/chinese',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_string('data_dir', 'data/chinese',
+tf.app.flags.DEFINE_string('data_dir', 'result/chinese_result/gc_ch2-1',
                            """Data directory""")
 tf.app.flags.DEFINE_integer('image_width', 48,
                             """Image Width.""")
@@ -521,63 +521,74 @@ def graphcut(linenet_manager, file_path):
 
 def postprocess():
     num_files = 0
-    sum_paths = 0
-    min_paths = 1000
-    max_paths = -1000
-    sum_diff_labels = 0
-    min_diff_labels = 100
-    max_diff_labels = -100
+    path_list = []
     diff_list = []
-    sum_duration = 0
-    min_duration = 10000
-    max_duration = -1
-
+    acc_list = []
+    duration_list = []
+    
     stat_path = os.path.join(FLAGS.data_dir, 'stat.txt')
     with open(stat_path, 'r') as f:
         while True:
             line = f.readline()
             if not line: break
+            elif line.find('total') > -1: break
             
-            name, num_labels, diff_labels, duration = line.split()
+            name, num_labels, diff_labels, accuracy, duration = line.split()
             num_labels = int(num_labels)
             diff_labels = int(diff_labels)
+            accuracy = float(accuracy)
             duration = float(duration)
             num_paths = num_labels - diff_labels
 
-            sum_paths = sum_paths + num_paths
-            if num_paths < min_paths:
-                min_paths = num_paths
-            if num_paths > max_paths:
-                max_paths = num_paths
-
-            sum_diff_labels = sum_diff_labels + abs(diff_labels)
-            if diff_labels < min_diff_labels:
-                min_diff_labels = diff_labels
-            if diff_labels > max_diff_labels:
-                max_diff_labels = diff_labels
             num_files = num_files + 1
-            diff_list.append(diff_labels)            
-            
-            sum_duration = sum_duration + duration
-            if duration < min_duration:
-                min_duration = duration
-            if duration > max_duration:
-                max_duration = duration
+            path_list.append(num_paths)
+            diff_list.append(diff_labels)
+            acc_list.append(accuracy)
+            duration_list.append(duration)
 
     # the histogram of the data
+    path_list = np.array(path_list)
     diff_list = np.array(diff_list)
+    acc_list = np.array(acc_list)
+    duration_list = np.array(duration_list)
+
+    max_paths = np.amax(path_list)
+    min_paths = np.amin(path_list)
+    avg_paths = np.average(path_list)
+    max_diff_labels = np.amax(diff_list)
+    min_diff_labels = np.amin(diff_list)
+    avg_diff_labels = np.average(np.abs(diff_list))
+    max_acc = np.amax(acc_list)
+    min_acc = np.amin(acc_list)
+    avg_acc = np.average(acc_list)
+    max_duration = np.amax(duration_list)
+    min_duration = np.amin(duration_list)
+    avg_duration = np.average(duration_list)
     
     bins = max_diff_labels - min_diff_labels + 1
     fig = plt.figure()
     weights = np.ones_like(diff_list)/float(len(diff_list))
-    # plt.hist(diff_list, bins=bins, color='blue', normed=False, alpha=0.75, weights=weights)
+    plt.hist(diff_list, bins=bins, color='blue', normed=False, alpha=0.75, weights=weights)
+    plt.xlim(min_diff_labels, max_diff_labels)
+    plt.ylim(0, 1)
+    plt.title('Histogram of Label Difference (normalized)')
+    plt.grid(True)
+    
+    fig.canvas.draw()
+    pred_hist = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    pred_hist = pred_hist.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    plt.close(fig)
+
+    hist_path = os.path.join(FLAGS.data_dir, 'label_diff_hist_norm.png')
+    scipy.misc.imsave(hist_path, pred_hist)
+
+    
+    fig = plt.figure()
     plt.hist(diff_list, bins=bins, color='blue', normed=False, alpha=0.75)
     plt.xlim(min_diff_labels, max_diff_labels)
-    # plt.ylim(0, 1)
     plt.title('Histogram of Label Difference')
     plt.grid(True)
     
-    # Now we can save it to a numpy array.
     fig.canvas.draw()
     pred_hist = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
     pred_hist = pred_hist.reshape(fig.canvas.get_width_height()[::-1] + (3,))
@@ -586,17 +597,52 @@ def postprocess():
     hist_path = os.path.join(FLAGS.data_dir, 'label_diff_hist.png')
     scipy.misc.imsave(hist_path, pred_hist)
 
+
+    fig = plt.figure()
+    weights = np.ones_like(acc_list)/float(len(acc_list))
+    plt.hist(acc_list, bins=bins, color='blue', normed=False, alpha=0.75, weights=weights)
+    # plt.hist(acc_list, bins=bins, color='blue', normed=False, alpha=0.75)
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.title('Histogram of Accuracy (normalized)')
+    plt.grid(True)
+    
+    fig.canvas.draw()
+    pred_hist = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    pred_hist = pred_hist.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    plt.close(fig)
+
+    hist_path = os.path.join(FLAGS.data_dir, 'accuracy_hist_norm.png')
+    scipy.misc.imsave(hist_path, pred_hist)
+
+    
+    fig = plt.figure()
+    plt.hist(acc_list, bins=bins, color='blue', normed=False, alpha=0.75)
+    plt.xlim(0, 1)
+    plt.title('Histogram of Accuracy')
+    plt.grid(True)
+    
+    fig.canvas.draw()
+    pred_hist = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    pred_hist = pred_hist.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    plt.close(fig)
+
+    hist_path = os.path.join(FLAGS.data_dir, 'accuracy_hist.png')
+    scipy.misc.imsave(hist_path, pred_hist)
+
+
     print('total # files: %d' % num_files)
-    print('min/max/avg. paths: %d,%d,%.3f' % (min_paths, max_paths, sum_paths/num_files))
-    print('min/max/avg. abs diff labels: %d,%d,%.3f' % (min_diff_labels, max_diff_labels, sum_diff_labels/num_files))
-    print('min/max/avg. duration (sec): %.3f,%.3f,%.3f' % (min_duration, max_duration, sum_duration/num_files))
+    print('min/max/avg. paths: %d, %d, %.3f' % (min_paths, max_paths, avg_paths))
+    print('min/max/avg. abs diff labels: %d, %d, %.3f' % (min_diff_labels, max_diff_labels, avg_diff_labels))
+    print('min/max/avg. accuracy: %.3f, %.3f, %.3f' % (min_acc, max_acc, avg_acc))
+    print('min/max/avg. duration (sec): %.3f, %.3f, %.3f' % (min_duration, max_duration, avg_duration))
     
     result_path = os.path.join(FLAGS.data_dir, '_result.txt')
     f = open(result_path, 'w')
-    f.write('total # files: %d\n' % num_files)
-    f.write('min/max/avg. paths: %d,%d,%.3f\n' % (min_paths, max_paths, sum_paths/num_files))
-    f.write('min/max/avg. abs diff labels: %d,%d,%.3f\n' % (min_diff_labels, max_diff_labels, sum_diff_labels/num_files))
-    f.write('min/max/avg. duration (sec): %.3f,%.3f,%.3f' % (min_duration, max_duration, sum_duration/num_files))
+    f.write('min/max/avg. paths: %d, %d, %.3f\n' % (min_paths, max_paths, avg_paths))
+    f.write('min/max/avg. abs diff labels: %d, %d, %.3f\n' % (min_diff_labels, max_diff_labels, avg_diff_labels))
+    f.write('min/max/avg. accuracy: %.3f, %.3f, %.3f\n' % (min_acc, max_acc, avg_acc))
+    f.write('min/max/avg. duration (sec): %.3f, %.3f, %.3f\n' % (min_duration, max_duration, avg_duration))    
     f.close()
 
 

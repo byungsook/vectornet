49b656be5917d5ec06ad37a8915550d56a2c88d2
byungsook <contact.byungsoo@gmail.com>
2017-01-10 14:55:02 +0100

    fix minor bug

--- graphcut_vec_hand.py
+++ graphcut_vec_hand.py
@@ -343,7 +343,6 @@ def graphcut(file_path):
     print('%s: %s, the number of labels %d' % (datetime.now(), file_name, num_labels))
     print('%s: %s, energy before optimization %.4f' % (datetime.now(), file_name, e_before))
     print('%s: %s, energy after optimization %.4f' % (datetime.now(), file_name, e_after))
-    print('%s: %s, accuracy computed, avg.: %.3f' % (datetime.now(), file_name, acc_avg))
 
 
     # save label map image

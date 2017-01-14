735da0edd3290a5d60bf24edef1df7d1726d50fd
byungsook <contact.byungsoo@gmail.com>
2017-01-14 13:51:38 +0100

    update gc vec hand

--- graphcut_vec_hand.py
+++ graphcut_vec_hand.py
@@ -221,12 +221,11 @@ def graphcut(file_path):
     # create managers
     start_time = time.time()
     print('%s: manager loading...' % datetime.now())
-
+    crop_size = -1
     if FLAGS.use_batch:
         dist = int(FLAGS.window_size * FLAGS.neighbor_sigma + 0.5)
         crop_size = 2 * dist + 1
-        # linenet_manager = LinenetManager([FLAGS.image_height, FLAGS.image_width], crop_size)
-        linenet_manager = LinenetManager([FLAGS.image_height, FLAGS.image_width])
+        linenet_manager = LinenetManager([FLAGS.image_height, FLAGS.image_width], crop_size)
     else:
         linenet_manager = LinenetManager([FLAGS.image_height, FLAGS.image_width])
     duration = time.time() - start_time
@@ -244,7 +243,6 @@ def graphcut(file_path):
     tf.gfile.MakeDirs(FLAGS.test_dir + '/tmp')
     if FLAGS.use_batch:
         prob_file_path = os.path.join(FLAGS.test_dir + '/tmp', file_name) + '_{id}.npy'
-        # linenet_manager.extract_save_crop(img, FLAGS.batch_size, prob_file_path)
         linenet_manager.extract_save(img, FLAGS.batch_size, prob_file_path)
     else:
         y_batch, _ = linenet_manager.extract_all(img)
@@ -320,22 +318,22 @@ def graphcut(file_path):
         for i in xrange(num_line_pixels-1):
             p1 = np.array([line_pixels[0][i], line_pixels[1][i]])
             pred_p1 = np.load(prob_file_path.format(id=i))
-            # rng = nb.radius_neighbors([p1])
-            # neighbor_list = rng[1][0]
-            # for rj, j in enumerate(neighbor_list): # ids
-            #     if j <= i:
-            #         continue
-            for j in xrange(i+1, num_line_pixels): # see entire neighbors
+            rng = nb.radius_neighbors([p1])
+            neighbor_list = rng[1][0]
+            for rj, j in enumerate(neighbor_list): # ids
+                if j <= i:
+                    continue
+            # for j in xrange(i+1, num_line_pixels): # see entire neighbors
                 p2 = np.array([line_pixels[0][j], line_pixels[1][j]])
                 pred_p2 = np.load(prob_file_path.format(id=j))
-                # rp2 = [center+p2[0]-p1[0],center+p2[1]-p1[1]]
-                # rp1 = [center+p1[0]-p2[0],center+p1[1]-p2[1]]
-                # pred = (pred_p1[rp2[0],rp2[1]] + pred_p2[rp1[0],rp1[1]]) * 0.5
-                pred = (pred_p1[p2[0],p2[1]] + pred_p2[p1[0],p1[1]]) * 0.5 # see entire neighbors
+                rp2 = [center+p2[0]-p1[0],center+p2[1]-p1[1]]
+                rp1 = [center+p1[0]-p2[0],center+p1[1]-p2[1]]
+                pred = (pred_p1[rp2[0],rp2[1]] + pred_p2[rp1[0],rp1[1]]) * 0.5
+                # pred = (pred_p1[p2[0],p2[1]] + pred_p2[p1[0],p1[1]]) * 0.5 # see entire neighbors
                 pred = np.exp(-0.5 * (1.0-pred)**2 / FLAGS.prediction_sigma**2)
 
-                # d12 = rng[0][0][rj]
-                d12 = LA.norm(p1-p2, 2) # see entire neighbors
+                d12 = rng[0][0][rj]
+                # d12 = LA.norm(p1-p2, 2) # see entire neighbors
                 spatial = np.exp(-0.5 * d12**2 / FLAGS.neighbor_sigma**2)
                 f.write('%d %d %f %f\n' % (i, j, pred, spatial))
 

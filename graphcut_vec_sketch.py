e185c5e4f0b02becb03c5792d5f422946b0ca4b6
byungsook <contact.byungsoo@gmail.com>
2017-01-16 17:34:33 +0100

    revert to use big neighbor

--- graphcut_vec_sketch.py
+++ graphcut_vec_sketch.py
@@ -245,22 +245,22 @@ def graphcut(linenet_manager, intersectnet_manager, file_path):
         for i in xrange(num_line_pixels-1):
             p1 = np.array([line_pixels[0][i], line_pixels[1][i]])
             pred_p1 = np.load(prob_file_path.format(id=i))
-            rng = nb.radius_neighbors([p1])
-            neighbor_list = rng[1][0]
-            for rj, j in enumerate(neighbor_list): # ids
-                if j <= i:
-                    continue
-            # for j in xrange(i+1, num_line_pixels): # see entire neighbors
+            # rng = nb.radius_neighbors([p1])
+            # neighbor_list = rng[1][0]
+            # for rj, j in enumerate(neighbor_list): # ids
+            #     if j <= i:
+            #         continue
+            for j in xrange(i+1, num_line_pixels): # see entire neighbors
                 p2 = np.array([line_pixels[0][j], line_pixels[1][j]])
                 pred_p2 = np.load(prob_file_path.format(id=j))
-                rp2 = [center+p2[0]-p1[0],center+p2[1]-p1[1]]
-                rp1 = [center+p1[0]-p2[0],center+p1[1]-p2[1]]
-                pred = (pred_p1[rp2[0],rp2[1]] + pred_p2[rp1[0],rp1[1]]) * 0.5
-                # pred = (pred_p1[p2[0],p2[1]] + pred_p2[p1[0],p1[1]]) * 0.5 # see entire neighbors
+                # rp2 = [center+p2[0]-p1[0],center+p2[1]-p1[1]]
+                # rp1 = [center+p1[0]-p2[0],center+p1[1]-p2[1]]
+                # pred = (pred_p1[rp2[0],rp2[1]] + pred_p2[rp1[0],rp1[1]]) * 0.5
+                pred = (pred_p1[p2[0],p2[1]] + pred_p2[p1[0],p1[1]]) * 0.5 # see entire neighbors
                 pred = np.exp(-0.5 * (1.0-pred)**2 / FLAGS.prediction_sigma**2)
 
-                d12 = rng[0][0][rj]
-                # d12 = LA.norm(p1-p2, 2) # see entire neighbors
+                # d12 = rng[0][0][rj]
+                d12 = LA.norm(p1-p2, 2) # see entire neighbors
                 spatial = np.exp(-0.5 * d12**2 / FLAGS.neighbor_sigma**2)
                 f.write('%d %d %f %f\n' % (i, j, pred, spatial))
 

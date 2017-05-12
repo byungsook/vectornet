4abf580dd22d8d4a98dcb4d91e86392e55fba77f
Byungsoo Kim <kimby@inf.ethz.ch>
2017-05-12 08:54:57 +0200

    compare potrace to ch2

--- compare_potrace.py
+++ compare_potrace.py
@@ -26,7 +26,7 @@ FLAGS = None
 
 def compare_potrace():
     num_files = 0
-    file_path_list = []        
+    file_path_list = []
     file_list_path = os.path.join(FLAGS.data_dir, FLAGS.file_list)
     with open(file_list_path, 'r') as f:
         while True:
@@ -91,7 +91,7 @@ def svgpre2bmp(file_path):
         s_img = Image.open(io.BytesIO(s_png))
         s = np.array(s_img)[:,:,3].astype(np.float) # / 255.0
         max_intensity = np.amax(s)
-        s = s / max_intensity        
+        s = s / max_intensity
         s = 1.0 - s
 
         file_name = os.path.splitext(os.path.basename(file_path))[0]
@@ -142,7 +142,7 @@ def compute_accuracy(file_path, svg_file_path):
     path_starts = []
     with open(svg_file_path, 'r') as f:
         svg = f.read()
-        num_paths = svg.count('path')        
+        num_paths = svg.count('path')
 
     for i in xrange(num_paths):
         svg_xml = et.fromstring(svg)
@@ -185,24 +185,32 @@ def get_stroke_list(file_path):
     stroke_list = []
     with open(file_path, 'r') as f:
         svg = f.read()
+
+    chinese1 = False
+    if chinese1:
         r = 0
         s = [1, -1]
         t = [0, -900]
+    else:
+        r = 0
+        s = [1, 1]
+        t = [0, 0]
 
-        svg = svg.format(
-            w=FLAGS.image_width, h=FLAGS.image_height,
-            r=r, sx=s[0], sy=s[1], tx=t[0], ty=t[1])
+    svg = svg.format(
+        w=FLAGS.image_width, h=FLAGS.image_height,
+        r=r, sx=s[0], sy=s[1], tx=t[0], ty=t[1])
 
-        svg_xml = et.fromstring(svg)
+    if chinese1:
+        svg_xml = ET.fromstring(svg)
         num_paths = len(svg_xml[0])
 
         for i in xrange(num_paths):
-            svg_xml =et.fromstring(svg)
+            svg_xml = ET.fromstring(svg)
             stroke = svg_xml[0][i]
             for c in reversed(xrange(num_paths)):
                 if svg_xml[0][c] != stroke:
                     svg_xml[0].remove(svg_xml[0][c])
-            svg_one_stroke = et.tostring(svg_xml, method='xml')
+            svg_one_stroke = ET.tostring(svg_xml, method='xml')
 
             y_png = cairosvg.svg2png(bytestring=svg_one_stroke)
             y_img = Image.open(io.BytesIO(y_png))
@@ -214,18 +222,45 @@ def get_stroke_list(file_path):
             # plt.show()
 
             stroke_list.append(y)
+    else:
+        id = 0
+        num_paths = 0
+        while id != -1:
+            id = svg.find('path id', id + 1)
+            num_paths = num_paths + 1
+        num_paths = num_paths - 1 # uncount last one
 
+        for i in reversed(xrange(num_paths)):
+            id = len(svg)
+            svg_one_stroke = svg
+            for c in xrange(num_paths):
+                id = svg_one_stroke.rfind('path id', 0, id)
+                if c != i:
+                    id_start = svg_one_stroke.rfind('>', 0, id) + 1
+                    id_end = svg_one_stroke.find('/>', id_start) + 2
+                    svg_one_stroke = svg_one_stroke[:id_start] + svg_one_stroke[id_end:]
+
+            y_png = cairosvg.svg2png(bytestring=svg_one_stroke.encode('utf-8'))
+            y_img = Image.open(io.BytesIO(y_png))
+            y = (np.array(y_img)[:,:,3] > 0)
+
+            # # debug
+            # y_img = np.array(y_img)[:,:,3].astype(np.float) / 255.0
+            # plt.imshow(y_img, cmap=plt.cm.gray)
+            # plt.show()
+
+            stroke_list.append(y)
     return stroke_list
 
 
 def init_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('data_dir',
-                default='data/chinese1', # 'data_tmp/gc_test',
+                default='data/chinese2', # 'data_tmp/gc_test',
                 help='data directory',
                 nargs='?') # optional arg.
     parser.add_argument('dst_dir',
-                    default='result/compare/chinese1', # 'data_tmp/gc_test',
+                    default='result/compare/chinese2', # 'data_tmp/gc_test',
                     help='destination directory',
                     nargs='?') # optional arg.
     parser.add_argument('file_list',
